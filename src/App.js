69ffa69a119d1f1f35609fadcc089287fe85ceec
milosst686 <milosst686@hotmail.com>
2023-03-12 15:56:39 +0100

    4. Open Web3Modal

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import { SignClient } from '@walletconnect/sign-client';
+import { Web3Modal } from '@web3modal/standalone';
 import { useEffect,useState } from 'react';
 import './App.css';
+
+const web3Modal = new Web3Modal({
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  standaloneChains: ["eip155:5"]
+})
 
 function App() {
   const [signClient, setSignClient] = useState();
@@ -17,6 +23,30 @@ function App() {
     }
   }
 
+  async function handleConnect(){
+    if(!signClient) throw Error("Cannot connect. Sign Client is not created");
+    try {
+      const proposalNamespace={
+        eip155:{
+          chains: ["eip155:5"],
+          methods: ["eth_sendTransaction"],
+          events: ["connect", "disconnect"]
+        }
+      };
+
+      const {uri} = await signClient.connect({
+        requiredNamespaces: proposalNamespace
+      });
+
+      if(uri){
+        web3Modal.openModal({uri});
+      }
+
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
   useEffect(()=> {
     if(!signClient){
       createClient();
@@ -26,6 +56,7 @@ function App() {
   return (
     <div className="App">
      <h1>Wallet Connect</h1>
+     <button onClick={handleConnect} disabled={!signClient}>Connect</button>
     </div>
   );
 }
