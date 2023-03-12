77f1f339e580ca3a0fb2977892a8a9bfce539353
milosst686 <milosst686@hotmail.com>
2023-03-12 15:40:06 +0100

    2.Added project3.Create client

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
+import { SignClient } from '@walletconnect/sign-client';
+import { useEffect,useState } from 'react';
 import './App.css';
 
 function App() {
+  const [signClient, setSignClient] = useState();
+
+  async function createClient(){
+    try {
+      const client = await SignClient.init({
+        projectId: process.env.REACT_APP_PROJECT_ID
+      })
+      console.log('client', client);
+      setSignClient(client);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(()=> {
+    if(!signClient){
+      createClient();
+    }
+  }, [signClient]);
+
   return (
     <div className="App">
      <h1>Wallet Connect</h1>
